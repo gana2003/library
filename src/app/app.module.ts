9335a2cfd5984633bb907dbcb3f77a53c93dddc1
gana2003 <meenakshigana0@gmail.com>
2022-07-04 12:45:46 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddbookComponent } from './addbook/addbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddbookComponent
   ],
   imports: [
     BrowserModule,
